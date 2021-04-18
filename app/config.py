d0dcb76c31d462c54923a56755f1392f09365b72
Oxana Khamidova <oxana.khamidova@zalando.de>
2021-04-18 20:48:21 +0200

    add cli commands to load data from CSV

--- app/config.py
+++ app/config.py
@@ -1,1 +1,6 @@
 SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/dott"
+
+IMPORT_CSV_FILES_LOCATION = "/home/okhamidova/Desktop/dott/"
+RIDES_FOLDER = "rides"
+DEPLOYMENTS_FOLDER = "deployments"
+PICKUPS_FOLDER = "pickups"
